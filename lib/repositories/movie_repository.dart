0e75fe9a5d8748fcbf767a2ae6d887032d3ad3ec
Georgi Tasev <georgi.tasev12@gmail.com>
2025-06-14 19:00:21 +0300

    Created a new dio for movie details and update on the repository

--- lib/repositories/movie_repository.dart
+++ lib/repositories/movie_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert'; // Add this import
 import 'package:movies_app/dio/movie.dart';
+import 'package:movies_app/dio/movie_details.dart';
 import 'package:movies_app/services/api.dart';
 
 class MovieRepository {
@@ -19,6 +20,30 @@ class MovieRepository {
       return data.map((json) => Movie.fromJson(json)).toList();
     } catch (e) {
       throw Exception('Failed to load movies: $e');
+    }
+  }
+
+  Future<MovieDetails?> getMovieDetailsById(String id) async {
+    try {
+      final response = await Api().dio.get('movies-coming-soon.json');
+
+      List<dynamic> data;
+      if (response.data is String) {
+        data = jsonDecode(response.data) as List<dynamic>;
+      } else if (response.data is List) {
+        data = response.data as List<dynamic>;
+      } else {
+        throw Exception('Unexpected response format');
+      }
+
+      final json = data.firstWhere(
+        (item) => item['id'] == id,
+        orElse: () => null,
+      );
+      if (json == null) return null;
+      return MovieDetails.fromJson(json);
+    } catch (e) {
+      throw Exception('Failed to load movie details: $e');
     }
   }
 }
