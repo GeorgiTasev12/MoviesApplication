23ddd1f02d50a300703393ecfd2bbc728b56cfeb
Georgi Tasev <georgi.tasev12@gmail.com>
2025-06-14 14:53:41 +0300

    Create a common image widget, the purpose of being reused in multiple pages.

---
+++ lib/common/common_url_image.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_app/views/home/cubit/home_cubit.dart';
+
+class CommonUrlImage extends StatelessWidget {
+  final int index;
+
+  const CommonUrlImage({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<HomeCubit, HomeState>(
+      buildWhen: (previous, current) => previous.movieList != current.movieList,
+      builder: (context, state) {
+        return ClipRRect(
+          borderRadius: BorderRadius.circular(8),
+          child: Image.network(
+            state.movieList?[index].posterUrl ?? '',
+            height: 450,
+            fit: BoxFit.cover,
+            loadingBuilder: (context, child, loadingProgress) {
+              if (loadingProgress == null) return child;
+              return Center(child: CircularProgressIndicator());
+            },
+            errorBuilder:
+                (context, error, stackTrace) => const Placeholder(
+                  child: Text(
+                    'Missing image.',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 255, 183, 177),
+                      fontSize: 18,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ),
+          ),
+        );
+      },
+    );
+  }
+}
